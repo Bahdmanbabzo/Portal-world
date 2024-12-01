86a7c60eaad353d8a0f698db4fc1f1340a2f524f
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-01 22:24:42 +0100

    Added background color and orbit controls.

--- src/components/3d/World.jsx
+++ src/components/3d/World.jsx
@@ -1,15 +1,16 @@
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from '../Model'; 
 
 function World (){
     return (
         <div className="h-screen w-screen">
             <Canvas>
+                <color attach="background" args={['#36454F']} />
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[0, 10, 5]} intensity={1} />
-                <mesh>
-                    <boxGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="orange" />
-                </mesh>
+                <OrbitControls />
+                <Model />
             </Canvas>
         </div>
 )};
