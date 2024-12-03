a9fe64beaf23f5c753899509c20a30dfe6828c0d
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-03 12:34:43 +0100

    Setting up fog.

--- src/components/3d/World.jsx
+++ src/components/3d/World.jsx
@@ -6,9 +6,9 @@ function World (){
     return (
         <div className="h-screen w-screen">
             <Canvas shadows>
-                <color attach="background" args={['#36454F']} />
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[0, 10, 5]} intensity={1} />
+                <color attach="background" args={['#343434']} />
+                <fogExp2 attach="fog" args={['#808080', 0.2]} />
+                <directionalLight position={[5, 5, 5]} intensity={3} />
                 <OrbitControls />
                 <Model />
             </Canvas>
