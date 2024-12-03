7af1db2b1497066b8744716be60841043422c6e6
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-03 12:04:34 +0100

    Large plane to act as floor.

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,10 +1,17 @@
 import { Stage } from '@react-three/drei'
 function Model() {
   return (
-    <Stage adjustCamera={false}>
+    <Stage 
+      adjustCamera={false}
+      intensity={0}
+    >
       <mesh>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="orange" />
+      </mesh>
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial color="gray" />
       </mesh>
     </Stage>
   )
