4419b0bb4fb9ba25c4dffb8f9d805ff41c2e257e
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-13 16:30:11 +0100

    Makes the portal visible from both sides.

--- src/components/3d/PortalMaker.jsx
+++ src/components/3d/PortalMaker.jsx
@@ -1,10 +1,11 @@
 import { MeshPortalMaterial } from "@react-three/drei";
-
+import * as THREE from "three";
 export default function PortalMaker({scene,cameraPosition}){
     return (
         <mesh>
-            <planeGeometry args={[40,40]} />
-            <MeshPortalMaterial>
+            <planeGeometry args={[2,3]} />
+            <MeshPortalMaterial side={THREE.DoubleSide}>
+                <perspectiveCamera position={cameraPosition}/>
                 {scene}
             </MeshPortalMaterial>
         </mesh>
