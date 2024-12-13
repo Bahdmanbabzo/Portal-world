b9226f1f333944e9c37bcfbb30ad803c5a1c65a1
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-12-13 07:52:39 +0100

    Float animation.

--- src/components/3d/Scene.jsx
+++ src/components/3d/Scene.jsx
@@ -1,4 +1,4 @@
-import { Stage, Cloud } from '@react-three/drei'
+import { Stage, Cloud, Float } from '@react-three/drei'
 import { useFrame, useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
@@ -11,17 +11,13 @@ function Model() {
 function Scene() {
   return (
     <group>
-      <Model />
-      <mesh castShadow position={[5,0,-20]}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
-      <mesh castShadow receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}>
-        <planeGeometry args={[100, 100]} />
-        <meshStandardMaterial color="gray" />
-      </mesh>
+      <Float
+      speed={3}
+      floatIntensity={3}>
+        <Model />
+      </Float>
       <Cloud 
-        bounds={[40, 1, 2]} 
+        bounds={[50, 1, 2]} 
         volume={60} 
         segments={40}
         color="#808080" 
